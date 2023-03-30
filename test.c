6ab5d4a34bbe450b33b1c808441a81bdd3cc196b
SeungHo0422 <songseungho9258@gmail.com>
2023-03-30 18:29:59 +0900

    SeungHo's 3rd commit

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	printf("hello Software Engineering!\n");
+	printf("hello my name is seungho!\n");
 	printf("seungho's 1st commit\n");
 	printf("seungho's 2nd commit\n");
 	return 0;
