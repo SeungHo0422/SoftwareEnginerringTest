29a8767dfe78b25b96065d8b9103ea7c782fc33b
Jimin <kaley0421@naver.com>
2023-03-30 18:28:25 +0900

    Jimin's 3rd Commit

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main() {
 	printf("hello Software Engineering!\n");
-	printf("seungho's 1st commit\n");
+	printf("JIMIN's 1st commit(ERRRORORORORORORORORO HERE!!!)\n");
 	printf("seungho's 2nd commit\n");
 
 	printf("Jimin's 1st commit\n");
